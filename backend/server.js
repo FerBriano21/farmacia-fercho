1c6ba778bc0df7e5f0b2dc5e9d25b4bd88b02bf8
Fernanda Briano <a20240069@utem.edu.mx>
2026-04-16 12:28:16 -0600

    redireaccionamiento

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
+
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../FRONTEND')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../FRONTEND/inicio.html'));
+});
+
 
 /* 🔐 LOGIN */
 app.post('/login', (req, res) => {
